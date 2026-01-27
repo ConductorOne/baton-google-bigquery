fbfa1b342cc500e549d03a46653c9fa1553fd586
Steve Gontzes <gontzess@gmail.com>
2026-01-27 15:28:57 -0500

    Upgrade baton-sdk to v0.7.4 and Go to 1.25.2

--- cmd/baton-google-bigquery/main.go
+++ cmd/baton-google-bigquery/main.go
@@ -8,6 +8,7 @@ import (
 	"github.com/conductorone/baton-google-bigquery/pkg/connector"
 	configSchema "github.com/conductorone/baton-sdk/pkg/config"
 	"github.com/conductorone/baton-sdk/pkg/connectorbuilder"
+	"github.com/conductorone/baton-sdk/pkg/connectorrunner"
 	"github.com/conductorone/baton-sdk/pkg/field"
 	"github.com/conductorone/baton-sdk/pkg/types"
 	"github.com/grpc-ecosystem/go-grpc-middleware/logging/zap/ctxzap"
@@ -32,6 +33,7 @@ func main() {
 		connectorName,
 		getConnector,
 		field.NewConfiguration(configurationFields),
+		connectorrunner.WithDefaultCapabilitiesConnectorBuilder(&connector.GoogleBigQuery{}),
 	)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err.Error())
